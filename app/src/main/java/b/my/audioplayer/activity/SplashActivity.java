a0c064cb6bca84132eb38caa5ffb41daab6729c6
SanjibBarma <sanjib.barma@v2.ltd>
2026-03-15 00:56:35 +0600

    logo

--- app/src/main/java/b/my/audioplayer/activity/SplashActivity.java
+++ app/src/main/java/b/my/audioplayer/activity/SplashActivity.java
@@ -1,9 +1,12 @@
 package b.my.audioplayer.activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.widget.VideoView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import b.my.audioplayer.R;
 
@@ -15,6 +18,19 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        VideoView videoView = findViewById(R.id.videoView);
+
+        String path = "android.resource://" + getPackageName() + "/" + R.raw.ic_app_logo;
+        Uri uri = Uri.parse(path);
+
+        videoView.setVideoURI(uri);
+
+        videoView.setOnPreparedListener(mp -> {
+            mp.setVolume(0f, 0f);   // sound mute
+            //mp.setLooping(true);
+        });
+
+        videoView.start();
 
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
             Intent intent = new Intent(SplashActivity.this, MainActivity.class);
